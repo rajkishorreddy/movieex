131fb64b0fe83fa13be421d2276b99f1685b31a0
rajkishorreddy <rajakishorbeeravalli@gmail.com>
2021-04-05 00:52:17 +0530

    main page designing

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import m from "../assets/m.png";
+const Footer = () => {
+  return (
+    <div>
+      <img className="footer-logo" src={m} alt="footer log" />
+    </div>
+  );
+};
+export default Footer;
--- client/src/components/Main.jsx
+++ client/src/components/Main.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Header from "./Header";
+import Footer from "./Footer";
 import "./scss/main.scss";
 import { connect } from "react-redux";
 import {
@@ -20,10 +21,104 @@ class Main extends React.Component {
     };
     fetchapi();
   }
+  renderNowPlayingMovies() {
+    if (!this.props.nowPlayingMovies) {
+      return <div>Loading...</div>;
+    } else {
+      return this.props.nowPlayingMovies.slice(0, 5).map((mv) => {
+        return (
+          <div className="mvt-ocard" key={mv.id}>
+            <div className="mvt-ocard-adult">{mv.adult ? "A" : "U/A"}</div>
+            <div className="mvt-icard">
+              <img src={mv.poster} alt="poster" className="mvt-icard-img"></img>
+            </div>
+            <div className="mvt-ocard-title">{mv.title}</div>
+            <div className="mvt-ocard-genre">
+              <div className="mvt-ocard-genre-item">love</div>
+              <div className="mvt-ocard-genre-item">action</div>
+              <div className="mvt-ocard-genre-item">thriller</div>
+            </div>
+            <div className="mvt-ocard-rating">{mv.rating}</div>
+            <div className="mvt-ocard-relese">{mv.release}</div>
+          </div>
+        );
+      });
+    }
+  }
+  renderTopRatedMovies() {
+    if (!this.props.topRatedMovies) {
+      return <div>Loading...</div>;
+    } else {
+      return this.props.topRatedMovies.slice(0, 5).map((mv) => {
+        return (
+          <div className="mvt-ocard" key={mv.id}>
+            <div className="mvt-ocard-adult">{mv.adult ? "A" : "U/A"}</div>
+            <div className="mvt-icard">
+              <img src={mv.poster} alt="poster" className="mvt-icard-img"></img>
+            </div>
+            <div className="mvt-ocard-title">{mv.title}</div>
+            <div className="mvt-ocard-genre">
+              <div className="mvt-ocard-genre-item">love</div>
+              <div className="mvt-ocard-genre-item">action</div>
+              <div className="mvt-ocard-genre-item">thriller</div>
+            </div>
+            <div className="mvt-ocard-rating">{mv.rating}</div>
+            <div className="mvt-ocard-relese">{mv.release}</div>
+          </div>
+        );
+      });
+    }
+  }
+  renderPopularTvShows() {
+    if (!this.props.PopularTv) {
+      return <div>Loading...</div>;
+    } else {
+      return this.props.PopularTv.slice(0, 5).map((mv) => {
+        return (
+          <div className="mvt-ocard" key={mv.id}>
+            {/* <div className="mvt-ocard-adult">{mv.adult ? "A" : "U/A"}</div> */}
+            <div className="mvt-icard">
+              <img src={mv.poster} alt="poster" className="mvt-icard-img"></img>
+            </div>
+            <div className="mvt-ocard-title">{mv.title}</div>
+            <div className="mvt-ocard-genre">
+              <div className="mvt-ocard-genre-item">love</div>
+              <div className="mvt-ocard-genre-item">action</div>
+              <div className="mvt-ocard-genre-item">thriller</div>
+            </div>
+            <div className="mvt-ocard-rating">{mv.rating}</div>
+            <div className="mvt-ocard-relese">{mv.release}</div>
+          </div>
+        );
+      });
+    }
+  }
   render() {
     return (
       <div>
         <Header />
+        <div className="mvt">
+          <div className="mvt-heading">Movies in theatres</div>
+          <div className="mvt-container">{this.renderNowPlayingMovies()}</div>
+          <a href="/movies" className="mvt-footer">
+            Explore more movies here !
+          </a>
+        </div>
+        <div className="mvt">
+          <div className="mvt-heading">Top Rated Movies</div>
+          <div className="mvt-container">{this.renderTopRatedMovies()}</div>
+          <a href="/movies" className="mvt-footer">
+            Explore more movies here !
+          </a>
+        </div>
+        <div className="mvt">
+          <div className="mvt-heading">Popular TV shows</div>
+          <div className="mvt-container">{this.renderPopularTvShows()}</div>
+          <a href="/movies" className="mvt-footer">
+            Explore more TV shows here !
+          </a>
+        </div>
+        <Footer />
       </div>
     );
   }
